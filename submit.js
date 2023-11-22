c27c932978e515091b559d4e4c83260cd9743eb6
Santiago <sacifuentespawk@gmail.com>
2023-11-21 23:18:23 -0500

    validation script begin

---
+++ submit.js
@@ -0,0 +1,21 @@
+// variables from the buttons
+const submitButton = document.querySelector('button[type="submit"]');
+const resetButton = document.querySelector('button[type="submit"]');
+
+// variables from the inputs
+
+const firstNameInput = document.querySelector('#firstName');
+const lastNameInput =   document.querySelector('#lastName');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
+const passwordInput = document.querySelector('#password');
+const confirmationInput = document.querySelector('#passwordConfirmation');
+
+// variables from the warnings
+
+const firstNameWarning = document.querySelector('#firstNameWarning');
+const lastNameWarning =   document.querySelector('#lastNameWarning');
+const emailWarning = document.querySelector('#emailWarning');
+const phoneWarning = document.querySelector('#phoneWarning');
+const passwordWarning = document.querySelector('#passwordWarning');
+const confirmationWarning = document.querySelector('#passwordConfirmationWarning');
